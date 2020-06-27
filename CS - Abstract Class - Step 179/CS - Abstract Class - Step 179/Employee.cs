fe5a14afc1f5c6b5d5639e3074c231c48ad84a05
daisysalem <62805119+daisysalem@users.noreply.github.com>
2020-06-27 10:35:17 -0700

    Question

---
+++ CS - Abstract Class - Step 179/CS - Abstract Class - Step 179/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CS___Abstract_Class___Step_179
+{
+    public class Employee : Person
+    {
+        public void SayName()
+        {
+            Console.WriteLine("Name: " + FirstName + " " + LastName);
+
+        }
+    }
+}
