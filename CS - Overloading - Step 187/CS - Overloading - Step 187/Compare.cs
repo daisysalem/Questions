529ce8fa4dd3a6c14289a7003f4fb2bf0bf6bf04
daisysalem <62805119+daisysalem@users.noreply.github.com>
2020-06-27 16:23:42 -0700

    Need help on Step 187

---
+++ CS - Overloading - Step 187/CS - Overloading - Step 187/Compare.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CS___Overloading___Step_187
+{
+    public class Compare
+    {
+        public static Check operator== (EmpId1, EmpId2)
+        {
+            if (EmpId1 == EmpId2)
+                Console.WriteLine("Id's match");
+            else
+                Console.WriteLine("Id's don't match"); 
+        }
+
+        public static Check operator!= (EmpId1, EmpId2) 
+        {
+            if (EmpId1 != EmpId2)
+                Console.WriteLine("Id's don't match");
+            else
+                Console.WriteLine("Id's match");
+        }
+
+    }
+
+    
+}
---
+++ CS - Overloading - Step 187/CS - Overloading - Step 187/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+Overload the "==" operator so it checks if two Employee objects are equal 
+by comparing their Id property.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace CS___Overloading___Step_187
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {   
+            Compare EmpId1 = new Compare(100);
+            Compare EmpId2 = new Compare(105);
+
+            Compare Check operator== (EmpId1, EmpId2);
+            Console.WriteLine(Compare.Check.operator==);
+        }
+    }
+}
+
